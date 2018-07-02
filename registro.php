b360a9552f36ca0ecda3dd5a645b70f02a16dbc8
Sebastián Riquelme V <searique@alumnos.ubiobio.cl>
2018-07-01 21:19:10 -0400

    nuevos parametros

--- registro.php
+++ registro.php
@@ -5,13 +5,19 @@
 		$nombre=$_POST['nombre'];
 		$correo=$_POST['correo'];
 		$nombreEmpresa=$_POST['nombreEmpresa'];
+
 		$areaEmpresa=$_POST['areaEmpresa'];
 		$tamanoEmpresa=$_POST['tamanoEmpresa'];
+		
+		$stackoverflow=$_POST['stackoverflow'];
+		$tiempo=$_POST['tiempo'];
+		$documentacion=$_POST['documentacion'];
+
 		$resultado=$_POST['resultado'];
 
 		
-			$sql="INSERT into resultados (nombre,correo,nombreEmpresa,areaEmpresa,tamanoEmpresa,resultadoFramework)
-				values ('$nombre','$correo','$nombreEmpresa','$areaEmpresa','$tamanoEmpresa','$resultado')";
+			$sql="INSERT into resultados (nombre,correo,nombreEmpresa,areaEmpresa,tamanoEmpresa,stackoverflow,tiempo,documentacion,resultadoFramework)
+				values ('$nombre','$correo','$nombreEmpresa','$areaEmpresa','$tamanoEmpresa','$stackoverflow','$tiempo','$documentacion','$resultado')";
 			echo $result=mysqli_query($conexion,$sql);
 
  ?>
